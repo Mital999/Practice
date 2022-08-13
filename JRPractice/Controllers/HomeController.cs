5c845ef30bc4da17ef593ed288ab55a155fc9b34
unknown <mitalgpatel999@gmail.com>
2022-08-13 14:08:37 +0530

    jpractice

---
+++ JRPractice/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using Newtonsoft.Json;
+
+namespace JRPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["con"].ConnectionString);
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult studentList()
+        {
+            string data ;
+            SqlCommand cmd = new SqlCommand("Get_Student_Data", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter adp = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adp.Fill(dt);
+            data = JsonConvert.SerializeObject(dt);
+            return Json(JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ JRPractice/Models/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JRPractice.Models
+{
+    public class Student
+    {
+       public int ID { get; set; }
+        public string StudentName { get; set; }
+        public string Address { get; set; }
+        public string Class { get; set; }
+        public int Age { get; set; }
+        public string Hobbies { get; set; }
+        public string Gender { get; set; }
+        public int State { get; set; }
+        public int City { get; set; }
+        public long Pincode { get; set; }
+        public string Photo { get; set; }
+
+    }
+    public class state
+    {
+       public int Stateid { get; set; }
+       public int Statename { get; set; }
+    }
+
+    public class city
+    {
+        public int Cityid { get; set; }
+        public string Cityname { get; set; }
+        public int Stateid { get; set; }
+    }
+}
